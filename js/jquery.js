c17111881381122597b23517c9e6b7bedc1f93e1
vagrant <vagrant@lighthouse-labs>
2014-01-14 06:33:40 +0000

    Called objects for results

--- js/jquery.js
+++ js/jquery.js
@@ -1,15 +1,45 @@
 $(document).ready(function() {
-	$(".search").on("click", "#search-btn", function(e){
-		e.preventDefault();
+	$(".search").on("click", "#search-btn", function()
+	{
+		event.preventDefault();
 		
-		var inputValue = $("#query").val().toUpperCase();
+		var inputValue = $('#query').val();
 		var url = 'http://autocomplete.wunderground.com/aq?&cb=?&query=';
 		
-		$.getJSON(url+inputValue, function(json){
+		$.getJSON(url+inputValue, function(json)
+		{
 			var RESULTS = json.RESULTS;
-			for(var i=0; i <= RESULTS.length; i++) {
-				var result = RESULTS[i];
-				$("#results").append('<p>' + result.name + '</p>');
+			for(var i=0; i < RESULTS.length; i++) 
+			{
+				var result = RESULTS[i];	
+				var url = 'http://api.wunderground.com/api/03170fbb5687fcf6/forecast' + result.l + '.json';
+				$('#results').append('<li data-url= ' + url +' >' + result.name  + '</li>');
+			}
+
+			console.log()
+			var list = $("li");
+
+			for (var j = 0; j < list.length; j++) 
+			{
+				var listItem = $(list[j]);
+				var url = listItem.data("url");
+
+				listItem.click(function () {
+					$.ajax({
+            type: "GET",
+            url: url,
+            dataType: "jsonp",
+            success: function (data) {
+            	//handle success case
+            	console.log(data);
+            }
+            error: function (data) {
+            	// do stuff on failure
+            }
+            
+            
+        	});
+				});
 			}
 		});
 	});
